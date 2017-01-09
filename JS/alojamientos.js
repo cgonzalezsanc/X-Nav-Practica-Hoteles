b49b15ac1869eff65295429c701422c1f7845589
cgonzalezsanc <carlosgonzalezsanc@gmail.com>
2017-01-09 19:46:25 +0100

    Version basica finalizada

--- JS/alojamientos.js
+++ JS/alojamientos.js
@@ -18,7 +18,7 @@ function guardar_datos() {
     // Compruebo si hay error y guardo el contenido
     repositorio.show(function (error, repo) {
         if (error) {
-            $("#botones-github").after("<p>Se ha producido un error</p>");
+            alert(error);
         } else {
             var contenido = JSON.stringify(colecciones);
             repositorio.write('master', fichero, contenido,
@@ -50,12 +50,13 @@ function cargar_datos() {
     // Compruebo si hay error y extraigo el contenido
     repositorio.show(function (error, repo) {
         if (error) {
-            $("#botones-github").after("<p>Se ha producido un error</p>");
+            alert(error);
         } else {
             repositorio.read('master', fichero, function(err, data) {
                     colecciones = JSON.parse(data);
                     // Se cargan todos los campos de las colecciones
                     mostrar_colecciones();
+                    alert("Éxito al cargar");
             });
         }  
     });
@@ -69,8 +70,8 @@ function form_guardar() {
                           + "<h3>Guardar datos en Github</h3>"
                           + "<input type='text' name='token' value='' id='token' placeholder='Token'"
                           + "size='36' />"
-                          + "<button type='button' id='guardar-github'><span class='glyphicon glyphicon-upload'></span></button>"
-                          + "</div>");
+                          + "<button type='button' id='guardar-github'><span class='glyphicon glyphicon-upload'>"
+                          + "</span></button></div>");
     $("div#token-form button#guardar-github").click(guardar_datos);
 }
 
@@ -81,8 +82,8 @@ function form_cargar() {
                           + "<h3>Cargar datos de Github</h3>"
                           + "<input type='text' name='token' value='' id='token' placeholder='Token'"
                           + "size='36' />"
-                          + "<button type='button' id='cargar-github'><span class='glyphicon glyphicon-download'></span></button>"
-                          + "</div>");
+                          + "<button type='button' id='cargar-github'><span class='glyphicon glyphicon-download'>"
+                          + "</span></button></div>");
     $("div#token-form button#cargar-github").click(cargar_datos);
 }
 
@@ -115,10 +116,10 @@ function mostrar_alojamientos_coleccion(coleccion) {
     } else {
         lista = lista + "<p>No se ha seleccionado ninguna colección</p>";
     }
-    $('.lista-alojamientos-coleccion').html(lista);
+    $('.alojamientos-coleccion').html(lista);
     // Habilito que se pueda seleccionar un alojamiento desde esta lista
-    $('.lista-alojamientos-coleccion li').click(mostrar_alojamiento);
-    $('.lista-alojamientos-coleccion li').hover(function() {
+    $('.alojamientos-coleccion li').click(mostrar_alojamiento);
+    $('.alojamientos-coleccion li').hover(function() {
         $(this).css({'cursor': 'pointer', 'background': '#E8E8E8'});
     }, function() {
         $(this).css('background', 'white');
@@ -150,6 +151,7 @@ function nombre_coleccion_repetido(nombre) {
     for (var i=0; i < colecciones.length; i++) {
         if (nombre == colecciones[i].nombre) {
             encontrado = true;
+            break;
         }
     }
     return encontrado;
@@ -183,11 +185,11 @@ function mostrar_colecciones() {
         }
         lista = lista + "</ul>";
     }
-    $('.lista-colecciones').html(lista);
+    $('.colecciones').html(lista);
     if (alojamientos != null) {
         // Handler para seleccionar una coleccion
-        $('.lista-colecciones li').click(seleccionar_coleccion);
-        $('.lista-colecciones li').hover(function() {
+        $('.colecciones li').click(seleccionar_coleccion);
+        $('.colecciones li').hover(function() {
             $(this).css({'cursor': 'pointer', 'background': '#E8E8E8'});
         }, function() {
             $(this).css('background', 'white');
@@ -201,7 +203,7 @@ function mostrar_formulario_añadir_colecc() {
           + "<input type='text' name='nombre_coleccion' placeholder='Nombre' "
           + "value='' id='nombre_coleccion' size='30' />"
           + "<button type='button' id='añadir_coleccion'><span class='glyphicon glyphicon-plus'></span></button>";
-    $('#form-nueva-coleccion').html(txt);
+    $('#nueva-coleccion').html(txt);
 }
 
 // Carga la interfaz de las colecciones
@@ -213,9 +215,9 @@ function mostrar_interfaz_colecciones() {
     mostrar_formulario_añadir_colecc();
     if (alojamientos != null) {
         // Handler para crear una nueva coleccion
-        $('#form-nueva-coleccion button#añadir_coleccion').click(añadir_coleccion);
+        $('#nueva-coleccion button#añadir_coleccion').click(añadir_coleccion);
         // Handler para añadir alojamientos a la coleccion seleccionada
-        $('div#colecciones .lista-total li').click(añadir_alojamiento_a_coleccion);
+        $('div#colecciones .alojamientos li').click(añadir_alojamiento_a_coleccion);
     }
 }
 
@@ -243,9 +245,9 @@ function fotos_carrusel(media) {
         if (i == 0) {
             txt = txt + ' active';
         }
-        txt = txt + '">';
-        txt = txt + '<img class="img-responsive center-block" src="' + media[i].url + '" alt="' + i.toString() + '">';
-        txt = txt + '</div>';
+        txt = txt + '">'
+              + '<img class="img-responsive center-block" src="' + media[i].url 
+              + '" alt="' + i.toString() + '">' + '</div>';
     }
     return txt + '</div>'
 }
@@ -267,9 +269,9 @@ function insertar_fotos(multimedia) {
     var carrusel = '</p><p>';
     var media = multimedia.media;
     if (media.length > 0) {
-        carrusel = carrusel + '<div class="carousel" class="carousel slide" data-ride="carousel" data-interval="5000">'
-        carrusel = carrusel + indicadores_carrusel(media) + fotos_carrusel(media) + controles_carrusel();
-	    carrusel = carrusel + '</div>'
+        carrusel = carrusel + '<div class="carousel" class="carousel slide" data-ride="carousel"' 
+                   + 'data-interval="5000">' + indicadores_carrusel(media) + fotos_carrusel(media) 
+	               + controles_carrusel() + '</div>';
     } else {
         carrusel = "";
     }
@@ -282,7 +284,8 @@ function comprobar_null(alojamiento, tipo) {
     var txt = "<p>";
     switch (tipo) {
         case "Direccion":
-            txt = txt + '<span data-toggle="tooltip" data-original-title="Dirección" class="glyphicon glyphicon-globe"></span> '
+            txt = txt + '<span data-toggle="tooltip" data-original-title="Dirección"'
+                  + ' class="glyphicon glyphicon-globe"></span> '
             try {
                 if (alojamiento.geoData.address != null) {
                     txt = txt + alojamiento.geoData.address + '</p>';
@@ -296,7 +299,8 @@ function comprobar_null(alojamiento, tipo) {
         case "Descripcion":
             try {
                 if (alojamiento.basicData.body != null) {
-                    txt = '<div style="text-align:justify">' + alojamiento.basicData.body + '</div>';
+                    txt = '<div style="text-align:justify">'
+                          + alojamiento.basicData.body + '</div>';
                 } else {
                     txt = "<p>Descripción no disponible</p>";
                 }
@@ -338,9 +342,11 @@ function comprobar_null(alojamiento, tipo) {
             }
             break;
         case "Calidad":
-            txt = txt + '<span data-toggle="tooltip" data-original-title="Estrellas" class="glyphicon glyphicon-star"></span> '
+            txt = txt + '<span data-toggle="tooltip" data-original-title="Estrellas"'
+                  + ' class="glyphicon glyphicon-star"></span> '
             try {
-                if (alojamiento.extradata.categorias.categoria.subcategorias.subcategoria.item[1]['#text'] != null) {
+                if (alojamiento.extradata.categorias.categoria
+                    .subcategorias.subcategoria.item[1]['#text'] != null) {
                     txt = txt + alojamiento.extradata.categorias.categoria
                                 .subcategorias.subcategoria.item[1]['#text'] + '</p>';
                 } else {
@@ -351,7 +357,8 @@ function comprobar_null(alojamiento, tipo) {
             }
             break;
         case "Telefono":
-            txt = txt + '<span data-toggle="tooltip" data-original-title="Teléfono" class="glyphicon glyphicon-earphone"></span> '
+            txt = txt + '<span data-toggle="tooltip" data-original-title="Teléfono"'
+                  + ' class="glyphicon glyphicon-earphone"></span> '
             try {
                 if (alojamiento.basicData.phone != null) {
                     txt = txt + alojamiento.basicData.phone + '</p>';
@@ -363,7 +370,8 @@ function comprobar_null(alojamiento, tipo) {
             }
             break;
         case "Email":
-            txt = txt + '<span data-toggle="tooltip" data-original-title="Email" class="glyphicon glyphicon-envelope"></span> '
+            txt = txt + '<span data-toggle="tooltip" data-original-title="Email"'
+                  + ' class="glyphicon glyphicon-envelope"></span> '
             try {
                 if (alojamiento.basicData.email != null) {
                     txt = txt + alojamiento.basicData.email + '</p>';
@@ -375,11 +383,13 @@ function comprobar_null(alojamiento, tipo) {
             }
             break;
         case "Pagina web":
-            txt = txt + '<span data-toggle="tooltip" data-original-title="Página web" class="glyphicon glyphicon-link"></span> '
+            txt = txt + '<span data-toggle="tooltip" data-original-title="Página web"'
+                  + ' class="glyphicon glyphicon-link"></span> '
             try {
                 if (alojamiento.basicData.web != null) {
                     url = alojamiento.basicData.web
-                    txt = txt + '<a href="' + url + '" style="color:#23527c;">' + url + '</a></p>';
+                    txt = txt + '<a href="' + url + '" style="color:#23527c;">' + url
+                          + '</a></p>';
                 } else {
                     txt = "";
                 }
@@ -406,7 +416,7 @@ function comprobar_null(alojamiento, tipo) {
 		- Descripción
 		- Carrusel con fotos
 */
-function mostrar_info_reducida(alojamiento) {
+function info_reducida(alojamiento) {
 	var nombre = alojamiento.basicData.name;
 	var direccion = comprobar_null(alojamiento, "Direccion");
 	var descripcion = comprobar_null(alojamiento, "Descripcion");
@@ -433,7 +443,7 @@ function mostrar_info_reducida(alojamiento) {
     - Descripción
     - Carrusel con fotos
 */
-function mostrar_info_completa(alojamiento) {
+function info_completa(alojamiento) {
     if (alojamiento != null) {
         var nombre = alojamiento.basicData.name;
         var lat = comprobar_null(alojamiento, "Latitud");
@@ -449,7 +459,8 @@ function mostrar_info_completa(alojamiento) {
 	    var infoCompleta = '<h2>' + nombre + '</h2>'
                            + '<h3>Descripcion</h3>'
                            + descripcion + '<br>'
-                           + '<p>' + '<span data-toggle="tooltip" data-original-title="Coordenadas GPS" class="glyphicon glyphicon-map-marker"></span> '
+                           + '<p>' + '<span data-toggle="tooltip" data-original-title="Coordenadas GPS"'
+                           + ' class="glyphicon glyphicon-map-marker"></span> '
                            + lat + 'W, ' + lon + 'N</p>'
 			               + direccion
                            + clasif
@@ -477,8 +488,8 @@ function añadir_marcador(alojamiento) {
 	 .openPopup()
      .on('click', function() {
         // Cuando se hace click se muestra la informacion
-        $('#info-aloj-reducida').html(mostrar_info_reducida(alojamiento));
-	    $('#info-aloj-completa').html(mostrar_info_completa(alojamiento));
+        $('#info-aloj-reducida').html(info_reducida(alojamiento));
+	    $('#info-aloj-completa').html(info_completa(alojamiento));
      });
 	map.setView([lat, lon], 15);
 }
@@ -489,8 +500,8 @@ function mostrar_alojamiento() {
 	// Me quedo con el objeto correspondiente a este alojamiento a partir del
 	// atributo del elemento seleccionado
  	var alojamiento = alojamientos[$(this).attr('no')];
-	$('#info-aloj-reducida').html(mostrar_info_reducida(alojamiento));
-	$('#info-aloj-completa').html(mostrar_info_completa(alojamiento));
+	$('#info-aloj-reducida').html(info_reducida(alojamiento));
+	$('#info-aloj-completa').html(info_completa(alojamiento));
     // Activo los tooltip
     $('[data-toggle="tooltip"]').tooltip();
 	añadir_marcador(alojamiento);
@@ -507,9 +518,9 @@ function mostrar_alojamientos() {
 	        lista = lista + '<li no=' + i + '>' + alojamientos[i].basicData.title + '</li>';
 	    }
         lista = lista + '</ul>';
-        $('.lista-total').html(lista);
-        $('.lista-total li').click(mostrar_alojamiento);
-        $('.lista-total li').hover(function() {
+        $('.alojamientos').html(lista);
+        $('.alojamientos li').click(mostrar_alojamiento);
+        $('.alojamientos li').hover(function() {
             $(this).css({'cursor': 'pointer', 'background': '#E8E8E8'});
         }, function() {
             $(this).css('background', 'white');
@@ -517,7 +528,7 @@ function mostrar_alojamientos() {
     } else {
         lista = lista + '<p>La funcionalidad está restringida si no cargas los alojamientos.</p>'
                 + '<button type="button" class="cargarJSON">Cargar Alojamientos</button>'
-        $('.lista-total').html(lista);
+        $('.alojamientos').html(lista);
     }
 }
 
@@ -525,7 +536,7 @@ function mostrar_alojamientos() {
 // ----------------------------------------GENERAL--------------------------------------------
 
 /* Carga los alojamientos del fichero alojamientos.json. Se genera una lista con todos ellos,
-   se muestran los botones para cargar y guardar la información y se carga la interfaz de las
+   se activan los botones para cargar y guardar la información y se carga la interfaz de las
    colecciones */
 function cargar_alojamientos() {
 	$.getJSON("JSON/alojamientos.json", function(data) {
@@ -549,12 +560,12 @@ $(document).ready(function() {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
-    // Inicializo la variable global alojamientos y alojamiento a null
+    // Inicializo la variable global alojamientos a null
     alojamientos = null;
     // Cargo la información de las tres pestañas
     mostrar_alojamientos();
     mostrar_interfaz_colecciones();
-    $('#info-aloj-completa').html(mostrar_info_completa(null));
+    $('#info-aloj-completa').html(info_completa(null));
     
 	/* Habilito el handler para que cuando se haga click en el elemento cargarJSON se carguen los
 	   alojamientos */
